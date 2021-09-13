fd6249a3d4e71b2991bbaca57df9f4542050e362
Kieran Lewis <kieranlewis1334@gmail.com>
2021-09-14 01:15:53 +1200

    Added clicking on the board to add a token to it

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const gameBoard = (() => {
+    const _board = [['','','X'],['','',''],['','','']]; //empty board
+    const _boardDiv = document.querySelector('.tictactoe-board');
+
     const getBoard = () => _board;
-    const _board = [['X','',''],['','',''],['','','']]; //empty board
-
     const render = () => {
-        const boardDiv = document.querySelector('.tictactoe-board');
-
         console.log('rendering board');
+        _boardDiv.textContent = ''; //delete current board
+
+        //loop through board and create div elements with the correct tokens
         for(let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
                 const div = document.createElement('div');
                 div.textContent = _board[i][j];
                 div.setAttribute('class', 'grid-item');
-                boardDiv.appendChild(div);
+                div.setAttribute('data-index', `${i},${j}`);
+                div.addEventListener('click', _addMove);
+
+                _boardDiv.appendChild(div);
             }
         }
+    }
+
+    const _addMove = (e) => {
+        let index = e.target.getAttribute('data-index').split(',');
+        const position = _board[index[0]][index[1]];
+        _board[index[0]][index[1]] = 'X'
+        console.log(_board);
+        render();
     }
 
     return {
@@ -25,7 +38,7 @@ const gameBoard = (() => {
 const game = (() => {
     const init = () => {
         console.log('initialising game');
-        const board = gameBoard.getBoard();
+        gameBoard.render();
         const player1 = Player('p1', 'X');
         const player2 = Player('p2', 'O');
     };
